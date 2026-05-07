52627b802181bcf242a0eba3bcc426376c180ed7
Michał Kosior crqch <48185508+crqch@users.noreply.github.com>
2026-05-07 11:06:26 +0200

    fix(backend): update seed function to properly add admin flag

--- backend/priv/repo/seeds.exs
+++ backend/priv/repo/seeds.exs
@@ -14,8 +14,5 @@ Sunder.Repo.insert!(%Sunder.Accounts.User{
   email: "admin",
   username: "admin",
   pass: Argon2.hash_pwd_salt("admin"),
+  flags: ["is_admin"]
 })
-
-
-with {:ok, user} <- Sunder.Repo.get_by(Sunder.Accounts.User, email: "admin"),
-     do: Sunder.Repo.update!(user, set: %{flags: {:is_admin}})
