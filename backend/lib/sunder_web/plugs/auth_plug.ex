45c38e96bb1571d1931ce9c060c2004fb291daf8
Michał Kosior crqch <48185508+crqch@users.noreply.github.com>
2026-05-14 08:43:58 +0200

    chore(backend): remove verbose logging in auth plug

--- backend/lib/sunder_web/plugs/auth_plug.ex
+++ backend/lib/sunder_web/plugs/auth_plug.ex
@@ -11,24 +11,22 @@ defmodule SunderWeb.Plugs.AuthPlug do
     conn = fetch_cookies(conn)
 
     with {:ok, token} <- Map.fetch(conn.cookies, "authorization"),
-          _ <- IO.inspect(token, label: "COOKIE TOKEN"),
-          %User{} = user <- Repo.one(
-            from(u in User,
-              join: ac in AccessToken,
-              on: ac.user_id == u.id,
-              where: ac.token == ^token,
-              select: u
-            )
-          ),
-          _ <- IO.inspect(user.id, label: "FOUND USER") do
-       assign(conn, :user, user)
-     else
-       err ->
-         IO.inspect(err, label: "AUTH FAILURE REASON")
-         conn
-         |> put_status(401)
-         |> json(%{message: "Missing/invalid cookie!"})
-         |> halt()
-     end
+         %User{} = user <-
+           Repo.one(
+             from(u in User,
+               join: ac in AccessToken,
+               on: ac.user_id == u.id,
+               where: ac.token == ^token,
+               select: u
+             )
+           ) do
+      assign(conn, :user, user)
+    else
+      err ->
+        conn
+        |> put_status(401)
+        |> json(%{message: "Missing/invalid cookie!"})
+        |> halt()
+    end
   end
 end
