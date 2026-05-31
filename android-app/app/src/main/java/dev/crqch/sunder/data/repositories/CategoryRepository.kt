4d7cc089c4d0cd1a5747577dc93b739fe91a111f
Michał Kosior crqch <48185508+crqch@users.noreply.github.com>
2026-05-31 19:47:40 +0200

    feat(android/data): implement CategoryRepository and enhance EntryRepository with filters

--- android-app/app/src/main/java/dev/crqch/sunder/data/repositories/CategoryRepository.kt
+++ android-app/app/src/main/java/dev/crqch/sunder/data/repositories/CategoryRepository.kt
@@ -1,4 +1,28 @@
 package dev.crqch.sunder.data.repositories
 
-class CategoryRepository {
+import dev.crqch.sunder.data.local.CategoryDao
+import dev.crqch.sunder.data.local.CategoryEntity
+import dev.crqch.sunder.utils.Cuid2
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+import javax.inject.Singleton
+
+
+@Singleton
+class CategoryRepository @Inject constructor(private val categoryDao: CategoryDao) {
+
+    val allCategories: Flow<List<CategoryEntity>> = categoryDao.getCategories()
+
+    suspend fun createCategory(title: String, description: String, color: String) {
+        val now = System.currentTimeMillis()
+        val category = CategoryEntity(
+            id = Cuid2.generate(),
+            title = title,
+            description = description,
+            color = color,
+            createdAt = now,
+            updatedAt = now
+        )
+        categoryDao.insert(category)
+    }
 }
--- android-app/app/src/main/java/dev/crqch/sunder/data/repositories/EntryRepository.kt
+++ android-app/app/src/main/java/dev/crqch/sunder/data/repositories/EntryRepository.kt
@@ -2,7 +2,9 @@ package dev.crqch.sunder.data.repositories
 
 import dev.crqch.sunder.data.local.EntryDao
 import dev.crqch.sunder.data.local.EntryEntity
+import dev.crqch.sunder.utils.Cuid2
 import kotlinx.coroutines.flow.Flow
+import java.time.Instant
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -10,7 +12,42 @@ import javax.inject.Singleton
 class EntryRepository @Inject constructor(private val entryDao: EntryDao) {
     val allEntries: Flow<List<EntryEntity>> = entryDao.getAllEntries()
 
-    fun getEntries(accountId: String): Flow<List<EntryEntity>> =
-        entryDao.getEntriesByAccountId(accountId)
+    fun getEntries(accountId: String? = null, categoryId: String? = null): Flow<List<EntryEntity>> {
+        return when {
+            accountId != null && categoryId != null -> {
+                entryDao.getEntriesByAccountAndCategoryId(accountId, categoryId)
+            }
 
+            accountId != null -> {
+                entryDao.getEntriesByAccountId(accountId)
+            }
+
+            categoryId != null -> {
+                entryDao.getEntriesByCategoryId(categoryId)
+            }
+
+            else -> {
+                allEntries
+            }
+        }
+    }
+
+    suspend fun createEntry(
+        title: String,
+        description: String,
+        amount: Float,
+        accountId: String,
+        categoryId: String,
+        date: Long
+    ) {
+        val now = Instant.now().toEpochMilli()
+        entryDao.upsert(
+            EntryEntity(
+                Cuid2.generate(),
+                title, date,
+                "",
+                amount, accountId, categoryId, description, createdAt = now, updatedAt = now
+            )
+        )
+    }
 }
