70a24f7b4316cb9bbac2b040d56ab1d03308b61c
Michał Kosior crqch <48185508+crqch@users.noreply.github.com>
2026-06-10 00:38:11 +0200

    fix(api): add missing openapi spec

--- backend/lib/sunder_web/controllers/sync_controller.ex
+++ backend/lib/sunder_web/controllers/sync_controller.ex
@@ -1,6 +1,107 @@
 defmodule SunderWeb.Authed.SyncController do
+  import SunderWeb.ApiMacros
   use SunderWeb, :controller
 
+  authed_operation(:sync,
+    summary: "Sync data",
+    parameters: [
+      last: [
+        in: :query,
+        schema: %{type: :string},
+        required: true,
+        description: "Last sync timestamp"
+      ]
+    ],
+    request_body: {
+      %{
+        type: :object,
+        properties: %{
+          accounts: %{type: :array, items: %{type: :object}},
+          categories: %{type: :array, items: %{type: :object}},
+          entries: %{type: :array, items: %{type: :object}}
+        }
+      },
+      [required: true, description: "Transactions to sync"]
+    },
+    responses: [
+      ok: {
+        %{
+          type: :object,
+          properties: %{
+            accounts: %{
+              type: :array,
+              items: %{
+                type: :object,
+                properties: %{
+                  id: %{type: :string},
+                  name: %{type: :string},
+                  eco_user_id: %{type: :string},
+                  deleted_at: %{type: :string, format: :"date-time", nullable: true},
+                  inserted_at: %{type: :string, format: :"date-time"},
+                  updated_at: %{type: :string, format: :"date-time"}
+                }
+              }
+            },
+            categories: %{
+              type: :array,
+              items: %{
+                type: :object,
+                properties: %{
+                  id: %{type: :string},
+                  title: %{type: :string},
+                  description: %{type: :string},
+                  color: %{type: :string},
+                  eco_user_id: %{type: :string},
+                  deleted_at: %{type: :string, format: :"date-time", nullable: true},
+                  inserted_at: %{type: :string, format: :"date-time"},
+                  updated_at: %{type: :string, format: :"date-time"}
+                }
+              }
+            },
+            entries: %{
+              type: :array,
+              items: %{
+                type: :object,
+                properties: %{
+                  id: %{type: :string},
+                  date: %{type: :string, format: :"date-time"},
+                  amount: %{type: :number},
+                  title: %{type: :string},
+                  description: %{type: :string},
+                  location: %{type: :string},
+                  account_id: %{type: :string},
+                  category_id: %{type: :string},
+                  eco_user_id: %{type: :string},
+                  deleted_at: %{type: :string, format: :"date-time", nullable: true},
+                  inserted_at: %{type: :string, format: :"date-time"},
+                  updated_at: %{type: :string, format: :"date-time"}
+                }
+              }
+            }
+          }
+        },
+        [description: "Synced data"]
+      },
+      bad_request: {
+        %{
+          type: :object,
+          properties: %{
+            error: %{type: :string}
+          }
+        },
+        [description: "Bad request"]
+      },
+      internal_server_error: {
+        %{
+          type: :object,
+          properties: %{
+            error: %{type: :string}
+          }
+        },
+        [description: "Internal server error"]
+      }
+    ]
+  )
   def sync(%{assigns: %{eco_user: eco_user, user: user}} = conn, %{"last" => timestamp} = params) do
     case SunderWeb.Sync.Pipeline.process(%{eco_user: eco_user, user: user}, params, timestamp) do
       {:ok, data} ->
